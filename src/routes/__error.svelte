15ec65e5e5eae6854f0c0e0e604fe4886b7e0154
James Bradbury <jamesbradbury93@gmail.com>
2021-08-17 14:15:10 +0100

    customise error apge

---
+++ src/routes/__error.svelte
@@ -0,0 +1,16 @@
+<script context="module">
+    export function load({ error, status }) {
+        return {
+            props: {
+                title: `${status}: ${error.message}`
+            }
+        };
+    }
+</script>
+
+<script>
+    export let title;
+</script>
+
+<h1>{title}</h1>
+<p>If you think this page should exist please contact James Bradbury</p>
