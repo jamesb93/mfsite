601a28fc82a46b5eb72be3cae9d7a626b1e5d82f
James Bradbury <jamesbradbury93@gmail.com>
2021-08-17 13:55:49 +0100

    add a layout for the whole site

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,6 +3,51 @@
 </script>
 
 <main class='container'>
-    <slot />
+    <div class='left'></div>
+    <div class='center'><slot/></div>
+    <div class='right'></div>
 </main>
+
+<style>
+	.container {
+		display: grid;
+		justify-content: center;
+		gap: 90px;
+	}
+
+    .left {
+        grid-area: left;
+    }
+
+    .center {
+        grid-area: center;
+    }
+
+    .right {
+        grid-area: right
+    }
+
+    	/* Media Queries */
+	@media (min-width: 1200px) {
+        .container {
+			grid-template-columns: auto min(80ch, 100%) auto;
+            grid-template-areas:
+            "left center right"
+        }
+    }
+    @media (max-width: 1200px) {
+        .container {
+			justify-content:start;
+			gap: 0px;
+			grid-template-rows: auto auto;
+            grid-template-areas:
+            "left"
+            "center"
+        }
+
+		.right {
+			display: none;
+		}
+    }
+</style>
 
