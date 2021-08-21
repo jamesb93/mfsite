3940089e78d993c6dee043e49d28b8376fe46a93
James Bradbury <jamesbradbury93@gmail.com>
2021-08-21 21:25:55 +0100

    yearpicker component

---
+++ src/lib/components/YearPicker.svelte
@@ -0,0 +1,23 @@
+<script>
+    const minYear = 1900;
+    const maxYear = new Date().getFullYear();
+    export let year = null;
+</script>
+
+<input 
+class='picker'
+type=number
+bind:value={year}
+/>
+
+<style>
+    input[type=number] {
+        -moz-appearance: textfield;
+    }
+
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+</style>
